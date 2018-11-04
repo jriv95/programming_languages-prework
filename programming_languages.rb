4f6a48dd781bbf0bf207d2abe7d694514d17ef9e
Joel Rivera <joel.rivera7995@gmail.com>
2018-11-04 13:17:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |key1, language|
+    language.each do |language_name, value|
+      if new_hash[language_name] == nil
+          new_hash[language_name] = value
+        end
+      new_hash[language_name][:key1] = []
+      new_hash[language_name][:key1] << style
+    end
+  end
+  new_hash[:javascript][:key1] << :oo
+  new_hash
 end
